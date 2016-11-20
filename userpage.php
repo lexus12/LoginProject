e5f05832674fb202f82bd2d34c738fecd21675a9
Krzysztof <krzysztofkorman83@gmail.com>
2016-11-20 12:02:42 +0100

    Update userpage.php

--- userpage.php
+++ userpage.php
@@ -1,27 +1,55 @@
+
+<?php
+session_start();
+
+class UserObject
+{
+		private $userName;
+		private $userEmail;
+		
+		public function __construct($userName, $userEmail)
+		{
+			
+			$this->userName = $userName;
+			$this->userEmail = $userEmail;
+			
+		}
+		
+		public function showUser()
+		{
+			
+			
+			echo strtoupper($this->userName);
+			
+		}
+			
+}//end of UserObject
+
+if(!isset($_SESSION['is_logged']) || $_SESSION['is_logged'] == false) {
+		//no logged user - redirect
+			$_SESSION['data_err'] = true;
+			header('Location: index.php');
+}
+
+include_once('classes/connect.class.php');
+include_once('classes/select.class.php');
+
+
+$userName = $_SESSION['nick'] ;	
+$userData = new UserObject($userName,'xxx');
+
+?>
+
 <html>
 	<header>
 		<title>My First Login Page</title>
-		<link rel="stylesheet" href="../css/style.css">
+		<link rel="stylesheet" href="css/style.css">
 	</header>
 	<body>
 	
-  
-		zalogowano
+		<h2><?php $userData->showUser(); ?> zalogowałeś się na konto!<br/></h2>
 		<a href="classes/logout.class.php">wyloguj</a>
 
-<?php
-
-//check if user is logged and show info
-if(isset($_SESSION['is_logged'] ) && $_SESSION['is_logged'] == true) {
-				
-	include_once('classes/userdata.class.php');
-				
-} else {
-  header('Location: index.php');
-  
-}
-
-?>
 		
 	</body>
 </html>	
