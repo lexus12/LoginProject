0fd4c7f21919ecd39a68b1d3ac747e6e77458cac
Krzysztof <krzysztofkorman83@gmail.com>
2016-11-20 09:58:32 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
+session_start();
 
-echo "HELLO GitHub";
+//check if user is logged and choose template
+if(isset($_SESSION['is_logged'] ) && $_SESSION['is_logged'] == true) {
+				
+	include_once('templates/header.is.logged.php');
+				
+} else {
+				
+	include_once('templates/header.isnt.logged.php');
+				
+}
+
+?>
+
+
+
+
+		
+	</body>
+</html>	
