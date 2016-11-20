cdbeace8ab45c479dff625852a091d20083484ce
Krzysztof <krzysztofkorman83@gmail.com>
2016-11-20 10:05:03 +0100

    Create header.is.logged.php
    a header file to view when user is logged

---
+++ templates/header.is.logged.php
@@ -0,0 +1,11 @@
+
+<html>
+	<header>
+		<title>My First Login Page</title>
+		<link rel="stylesheet" href="css/style.css">
+	</header>
+	<body>
+	
+  
+		zalogowano
+		<a href="../class/logout.class.php">wyloguj</a>
