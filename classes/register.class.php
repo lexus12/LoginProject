3034df3cfc6f94dba46860b62a8934919f6efb5a
Krzysztof <krzysztofkorman83@gmail.com>
2016-11-20 10:18:29 +0100

    Create register.class.php
    adds user to database based on data from register.php form

---
+++ classes/register.class.php
@@ -0,0 +1,109 @@
+<?php
+session_start();
+
+include_once('connect.class.php');
+
+class Register
+{
+	
+	private $nick;
+	private $password;
+	private $password2;
+	private $email;
+	private $conn;
+	
+	public function __construct($nick, $password, $password, $password2, $email, $conn)
+	{
+		
+		$this->nick = $nick;
+		$this->password = sha1($password);
+		$this->password2 = sha1($password2);
+		$this->email = $email;
+		$this->conn = $conn;
+		
+	}
+	
+	public function checkData()
+	{
+		//check  password
+		if($this->password == '' || strlen($this->password) < 6) {
+			$_SESSION['pass_err'] = true;
+			header('Location: ../register.php');
+			break;
+		} elseif($this->password != $this->password2) {
+			
+			$_SESSION['pass_err2'] = true;
+			header('Location: ../register.php');
+			break;
+		}
+				
+		//validate email
+		if (!filter_var($this->email, FILTER_VALIDATE_EMAIL)) {
+			$_SESSION['email_err'] = true;
+			header('Location: ../register.php');
+			break;
+		}
+		
+		
+		
+	}//end of checkData function
+	
+	public function addUser()
+	{
+		
+		$user = $this->conn->prepare("SELECT * FROM user WHERE nick = :nick OR email = :email");
+		
+		$user->bindValue(':nick' , $this->nick, PDO::PARAM_STR);
+		$user->bindValue(':email' , $this->email, PDO::PARAM_STR);
+		$user->execute();
+		
+		if ($user->rowCount()) {
+			
+			$_SESSION['user_exist_err'] = true;
+			header('Location: ../register.php');
+			
+			
+		} else {
+			
+			$this->privl = 1;
+			$ins = $this->conn->prepare("INSERT INTO user (nick, password, email, user_privilege) VALUES (:nick, :password, :email, :privl)");
+		
+			$ins->bindValue(':nick' , $this->nick, PDO::PARAM_STR);
+			$ins->bindValue(':password' , $this->password, PDO::PARAM_STR);
+			$ins->bindValue(':email' , $this->email, PDO::PARAM_STR);
+			$ins->bindValue(':privl' , $this->privl, PDO::PARAM_INT);
+			$ins->execute();
+			
+			if ($ins->rowCount() < 1) {
+			
+			$_SESSION['register_err'] = true;
+			header('Location: ../register.php');
+					
+			}
+			
+			$_SESSION['user_registered'] = true;
+			header('Location: ../index.php');
+			
+		}
+		
+	}//end of addUser
+	
+}//end of Register class
+
+
+if(isset($_POST['zarejestruj'])) {
+	
+	$nick = $_POST['nick'];
+	$password = $_POST['password'];
+	$password2 = $_POST['password2'];
+	$email = $_POST['email'];
+
+	
+	$login = new Register($nick, $password, $password, $password2, $email, $conn);
+	$login->checkData();
+	
+	$login->addUser();
+
+}
+
+?>
