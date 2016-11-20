415693b2e2d1d1e5119e8cdfa1f4cef783252bf9
Krzysztof <krzysztofkorman83@gmail.com>
2016-11-20 10:07:49 +0100

    Create register.php

---
+++ register.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+?>
+
+<html>
+	<header>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+		<link rel="stylesheet" href="css/style.css">
+	</header>
+	<body>
+	
+		  <div class="container">
+				  <div class="login">
+					<h1 class="login-heading">
+					  <strong>Witaj</strong> Zarejestruj się</h1>
+					  <form action="class/register.class.php" method="post">
+						<input type="text" name="nick" placeholder="NICK" required="required" class="input-txt" />
+						  <input type="password" name="password" placeholder="HASŁO" required="required" class="input-txt" />
+						  <input type="password" name="password2" placeholder="POWTÓRZ HASŁO" required="required" class="input-txt" />
+						  <input type="text" name="email" placeholder="E-MAIL" required="required" class="input-txt" />
+						  <div class="login-footer">
+							
+							<button type="submit" class="btn btn--right" name="zarejestruj">Zarejestruj </button>
+							<div class="comm">
+									<?php
+                  //adds communicats to user
+                  include_once('class/communicats.class.php'); 
+                  ?>
+							</div>
+						  </div>
+					  </form>
+				  </div>
+			</div>
+  
+    <script src="js/index.js"></script>
+
+<html>
+	<body>
+
+		
