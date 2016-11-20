db6874886feb15e698b2103f1246bdea762aa48f
Krzysztof <krzysztofkorman83@gmail.com>
2016-11-20 10:27:10 +0100

    Update header.isnt.logged.php
    directory change from class to classes

--- templates/header.isnt.logged.php
+++ templates/header.isnt.logged.php
@@ -9,7 +9,7 @@
 				  <div class="login">
 					<h1 class="login-heading">
 					  <strong>Witaj</strong> Zaloguj się</h1>
-					  <form action="class/login.class.php" method="post">
+					  <form action="classes/login.class.php" method="post">
 						<input type="text" name="nick" placeholder="LOGIN" required="required" class="input-txt" />
 						  <input type="password" name="password" placeholder="HASŁO" required="required" class="input-txt" />
 						  <div class="login-footer">
@@ -23,7 +23,7 @@
 								<div class="comm">
 									<?php
                   //adds a comunicats when user need to know something
-                  include_once('class/communicats.class.php');
+                  include_once('classes/communicats.class.php');
                   ?>
 								</div>
 						  </div>
