79906ff4371496082a22bad5cba441aa19aa11f7
Krzysztof <krzysztofkorman83@gmail.com>
2016-11-21 15:18:23 +0100

    Update profile.class.php
    Changes to make file work propertly

--- classes/profile.class.php
+++ classes/profile.class.php
@@ -1,3 +1,40 @@
 <?php
 
-echo "profile edition";
+session_start();
+
+class ChangeNick
+{
+		private $userName;
+			
+		public function __construct($userName)
+		{
+			
+			$this->userName = $userName;
+				
+		}
+		
+		public function makeChange()
+		{
+			
+			include_once('changename.class.php');
+			$change = new Change($this->userName);
+			$change->changeUserName();
+			
+		}
+			
+}//end of UserObject
+
+if(!isset($_SESSION['is_logged']) || $_SESSION['is_logged'] == false) {
+		//no logged user - redirect
+			$_SESSION['data_err'] = true;
+			header('Location: index.php');
+}
+
+
+if(isset($_POST['changeNick'])) {
+	
+	$userName = $_POST['nick'];
+	$changeNick = new ChangeNick($userName);
+	$changeNick->makeChange();
+	
+}
