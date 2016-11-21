2b96d9aa711e1ef2ee894edaf5b4220327c7580d
Krzysztof <krzysztofkorman83@gmail.com>
2016-11-21 13:38:54 +0100

    Update userpage.php
    Correct readability by spliting a file

--- userpage.php
+++ userpage.php
@@ -1,45 +1,6 @@
-
 <?php
-session_start();
-
-class UserObject
-{
-		private $userName;
-		private $userEmail;
-		
-		public function __construct($userName, $userEmail)
-		{
-			
-			$this->userName = $userName;
-			$this->userEmail = $userEmail;
-			
-		}
-		
-		public function showUser()
-		{
-			
-			
-			echo strtoupper($this->userName);
-			
-		}
-			
-}//end of UserObject
-
-if(!isset($_SESSION['is_logged']) || $_SESSION['is_logged'] == false) {
-		//no logged user - redirect
-			$_SESSION['data_err'] = true;
-			header('Location: index.php');
-}
-
-include_once('classes/connect.class.php');
-
-$userName = $_SESSION['nick'] ;	
-$userEmail = $_SESSION['email'];
-$userPrivil = $_SESSION['user_privilege'];
-$userData = new UserObject($userName, $userEmail);
-
+include_once('classes/userdata.class.php');
 ?>
-
 <html>
 	<header>
 		<title>My First Login Page</title>
