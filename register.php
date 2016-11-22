4aeba330da8b33affb343dbe86afec1dbc3b7396
Krzysztof <krzysztofkorman83@gmail.com>
2016-11-22 10:12:27 +0100

    Update register.php

--- register.php
+++ register.php
@@ -4,7 +4,6 @@ session_start();
 
 <html>
 	<header>
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
 		<link rel="stylesheet" href="css/style.css">
 	</header>
 	<body>
@@ -12,29 +11,34 @@ session_start();
 		  <div class="container">
 				  <div class="login">
 					<h1 class="login-heading">
-					  <strong>Witaj</strong> Zarejestruj się</h1>
+					  <strong>WELCOME - </strong> Register new account</h1>
 					  <form action="classes/register.class.php" method="post">
 						<input type="text" name="nick" placeholder="NICK" required="required" class="input-txt" />
-						  <input type="password" name="password" placeholder="HASŁO" required="required" class="input-txt" />
-						  <input type="password" name="password2" placeholder="POWTÓRZ HASŁO" required="required" class="input-txt" />
+						  <input type="password" name="password" placeholder="PASSWORD" required="required" class="input-txt" />
+						  <input type="password" name="password2" placeholder="REPEAT PASSWORD" required="required" class="input-txt" />
 						  <input type="text" name="email" placeholder="E-MAIL" required="required" class="input-txt" />
 						  <div class="login-footer">
 							
-							<button type="submit" class="btn btn--right" name="zarejestruj">Zarejestruj </button>
+							<button type="submit" class="btn btn--right" name="zarejestruj">Register </button>
 							<div class="comm">
 									<?php
-                  //adds communicats to user
-                  include_once('classes/communicats.class.php'); 
-                  ?>
-							</div>
+										//adds a comunicats when user need to know something
+										include_once('classes/communicats.class.php');
+									?>
+								</div>
+								<div class="err">
+									<?php
+										//adds a comunicats when user need to know something
+										include_once('classes/errors.class.php');
+									?>
+								</div>
 						  </div>
 					  </form>
 				  </div>
 			</div>
   
-    <script src="js/index.js"></script>
+	</body>
 
-<html>
-	<body>
+</html>
 
 		
