f2b58fb5919f4082b6bf684c7fa38b1a89a38ae0
Krzysztof <krzysztofkorman83@gmail.com>
2016-11-22 06:38:17 +0100

    Update profile.class.php
    This change will make a changing values more simple

--- classes/profile.class.php
+++ classes/profile.class.php
@@ -1,40 +1,10 @@
 <?php
-
 session_start();
-
-class ChangeNick
-{
-		private $userName;
-			
-		public function __construct($userName)
-		{
-			
-			$this->userName = $userName;
-				
-		}
-		
-		public function makeChange()
-		{
-			
-			include_once('changename.class.php');
-			$change = new Change($this->userName);
-			$change->changeUserName();
-			
-		}
-			
-}//end of UserObject
-
-if(!isset($_SESSION['is_logged']) || $_SESSION['is_logged'] == false) {
-		//no logged user - redirect
-			$_SESSION['data_err'] = true;
-			header('Location: index.php');
-}
-
-
+//page will get a values to change and redirect to proper class
 if(isset($_POST['changeNick'])) {
 	
-	$userName = $_POST['nick'];
-	$changeNick = new ChangeNick($userName);
-	$changeNick->makeChange();
+	$_SESSION['nick2change'] = $_POST['nick'];
+	header('Location: changename.class.php');
 	
 }
+
