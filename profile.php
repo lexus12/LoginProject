ae0ed835f14fb153960ab0842d15f7b88e7659cb
Krzysztof <krzysztofkorman83@gmail.com>
2016-11-22 11:45:29 +0100

    Update profile.php

--- profile.php
+++ profile.php
@@ -7,15 +7,23 @@ session_start();
 		<link rel="stylesheet" href="css/style.css">
 	</header>
 	<body>
-		<div class="info">
+		<div class="header">
+		
+		<div>
+				<a href="userpage.php" class="lnk">Back to User Page</a>
+				---------------------
+				<a href="classes/logout.class.php" class="lnk">Logout</a>
+			</div>
+		
+		</div>
+		
+		<div>
 			<div class="container">
-				 
-				  <div>
-					<h1 class="login-heading"><strong>Change your profile</strong></h1>
+				 <div>
+					<h1 class="info"><strong>Change your profile</strong></h1>
 						<div class="lnk">
-						<b>Change Your Nick:</b><br/>
+						<b>Change Your Nick - </b>( Current Nick is: <b><?php echo $_SESSION['nick']; ?></b> )<br/>
 						 <form action="classes/profile.class.php" method="post">
-							Current Nick: <b><?php echo $_SESSION['nick'], ': '; ?></b>
 							<input type="text" name="nick" placeholder="NEW LOGIN" required="required" class="input-txt"/>
 							   <div class="login-footer">
 									<button type="submit" class="btn btn--right" name="changeNick">Change Your Nick</button>
@@ -39,9 +47,9 @@ session_start();
 					
 					<div>
 						<div class="lnk">
-							<b>Change Your Email:</b><br/>
+							<b>Change Your Email - </b>( Current E-MAIL is: <b><?php echo $_SESSION['email']; ?></b> )<br/>
 							<form action="classes/profile.class.php" method="post">
-							Current E-MAIL: <b><?php echo $_SESSION['email'], ': '; ?></b>
+							
 							<input type="text" name="email" placeholder="NEW E-MAIL" required="required" class="input-txt"/>
 							<div class="login-footer">
 								<button type="submit" class="btn btn--right" name="changeEmail">Change Your E-MAIL</button>
@@ -64,13 +72,6 @@ session_start();
 				 
 			</div>
 			
-			<div>
-				<a href="userpage.php" class="lnk">Back to User Page</a>
-			</div>
-			
-			<div>
-				<a href="classes/logout.class.php" class="lnk">Logout</a>
-			</div>
 		</div>
 	</body>
 </html>
